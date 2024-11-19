f8021bf40fd34359be0fa256863a255d95ffa7a6
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2024-11-18 22:22:50 -0600

    Removing debug.php.
    Signed-off-by: Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>

--- includes/loader.php
+++ includes/loader.php
@@ -1,6 +1,5 @@
 <?PHP
 require_once(__DIR__ . '/config.php');
-if (DEBUG === true) require_once(__DIR__ . '/debug.php');
 require_once(__DIR__ . '/globals.php');
 require_once(__DIR__ . '/template_functions.php');
 require_once(__DIR__ . '/functions.php');
