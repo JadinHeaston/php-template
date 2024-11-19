6fbc3188ec2040931b5d2f620107c6d317739ef8
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2024-11-18 22:21:34 -0600

    Moving essential template functions to a separate file.
    Signed-off-by: Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>

--- includes/loader.php
+++ includes/loader.php
@@ -2,5 +2,6 @@
 require_once(__DIR__ . '/config.php');
 if (DEBUG === true) require_once(__DIR__ . '/debug.php');
 require_once(__DIR__ . '/globals.php');
+require_once(__DIR__ . '/template_functions.php');
 require_once(__DIR__ . '/functions.php');
 require_once(__DIR__ . '/models.php');
