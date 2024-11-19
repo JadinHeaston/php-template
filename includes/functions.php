8550748068ad0da8c86d8528c40e88559e1b802a
Jadin Heaston <jadin.heaston@como.gov>
2024-11-19 14:49:45 -0600

    Adding auth functionality.
    Signed-off-by: Jadin Heaston <jadin.heaston@como.gov>

--- includes/functions.php
+++ includes/functions.php
@@ -1,1 +1,22 @@
 <?php
+
+/**
+ * Custom Auth function that should return whether the user is authorized or not.
+ * A failed auth results in reauth() being called.
+ *
+ * @return boolean
+ */
+function auth(): bool
+{
+	return true;
+}
+
+/**
+ * Custom function that is triggered upon a failed auth().
+ *
+ * @return boolean
+ */
+function reauth(): void
+{
+	exit(1);
+}
--- includes/loader.php
+++ includes/loader.php
@@ -4,3 +4,6 @@ require_once(__DIR__ . '/globals.php');
 require_once(__DIR__ . '/template_functions.php');
 require_once(__DIR__ . '/functions.php');
 require_once(__DIR__ . '/models.php');
+
+if (auth() === false)
+	reauth();
