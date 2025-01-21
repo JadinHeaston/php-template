b121ed66184e222a8ea609591f7e2df944825714
Jadin Heaston <jadin.heaston@como.gov>
2025-01-21 12:56:25 -0600

    Moving models.php to be loaded before the config.
    Signed-off-by: Jadin Heaston <jadin.heaston@como.gov>

--- includes/loader.php
+++ includes/loader.php
@@ -1,9 +1,9 @@
 <?php
+require_once(__DIR__ . '/models.php');
 require_once(__DIR__ . '/config.php');
 require_once(__DIR__ . '/globals.php');
 require_once(__DIR__ . '/template_functions.php');
 require_once(__DIR__ . '/functions.php');
-require_once(__DIR__ . '/models.php');
 
 if (((isset($GLOBALS['disable_auth']) && $GLOBALS['disable_auth'] !== true) && auth() === false))
 	reauth();
