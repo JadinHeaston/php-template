aa5379c56896b499b11113a3fce75124fe7510af
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2025-01-24 19:47:39 -0600

    Adding jQuery and Select2.
    Signed-off-by: Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,43 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-
+document.addEventListener("DOMContentLoaded", async function () {
+	initializeListeners();
+	initSelect2Inputs();
 });
+
+async function initializeListeners() {
+}
+
+document.addEventListener('htmx:beforeHistorySave', async function (event) {
+	destroySelect2();
+});
+
+document.addEventListener('htmx:afterSettle', async function (event) {
+	initSelect2Inputs();
+});
+
+document.addEventListener('htmx:historyRestore', async function (event) {
+	initSelect2Inputs();
+});
+
+/*
+* Initializes all select2 inputs.
+*/
+async function initSelect2Inputs() {
+	document.querySelectorAll('select.select2').forEach(async (element) => {
+		let dropdownParent = jQuery(element).closest('[popover]');
+		if (dropdownParent.length === 0)
+			dropdownParent = document.body;
+
+		jQuery(element).select2({
+			dropdownParent: dropdownParent
+		});
+	});
+}
+
+/*
+* Destroys select2 inputs.
+*/
+async function destroySelect2() {
+	document.querySelectorAll('select.select2').forEach(async (element) => {
+		jQuery(element).select2('destroy');
+	});
+}
